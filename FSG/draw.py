ce9570edf40679c92df10c7aafec763846b3fa99
Saptarshi Dutta <quantumcosmos7@gmail.com>
2020-09-12 16:54:36 +0530

    REFACTOR_1: drawing tools moved to 'draw.py'

---
+++ FSG/draw.py
@@ -0,0 +1,51 @@
+from math import pi, sin, cos
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+class drawGL:
+    def __init__(self, adjustX, adjustY, increment):
+        self.adjustX = adjustX
+        self.adjustY = adjustY
+        self.inc = increment
+        pass
+
+    def circle(self, radius, posx, posy, n, epoch=0, slides=100):
+        glLineWidth(0.05)
+        glColor3f(0.4, 0.4, 0.4)
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_LINE_SMOOTH)
+        glBegin(GL_LINE_STRIP)
+        x = y = 0
+        for i in range(1, slides):
+            x = (radius/self.adjustX * cos(n*2*pi*i/slides + epoch) + posx)
+            y = (radius/self.adjustY * sin(n*2*pi*i/slides + epoch) + posy)
+            glVertex2f(x, y)
+        glEnd()
+        glColor3f(1.0, 1.0, 1.0)
+
+
+    def point(self, x, y, size=2.0):
+        glEnable(GL_POINT_SMOOTH)
+        glEnable(GL_BLEND)
+        glPointSize(size)
+        glBegin(GL_POINTS)
+        glVertex2f(x, y)
+        glEnd()
+
+
+    def line(self, a, b, width=1):
+        glLineWidth(width)
+        glBegin(GL_LINES)
+        glVertex2f(a[0], a[1])
+        glVertex2f(b[0], b[1])
+        glEnd()
+        glLineWidth(1)
+
+
+    def curve(self, l):
+        glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+        glEnable(GL_LINE_SMOOTH)
+        glBegin(GL_LINE_STRIP)
+        for i in range(len(l)):
+            glVertex2f(i*self.inc*0.05, l[i])
+        glEnd()
--- FSG/fourier.py
+++ FSG/fourier.py
@@ -8,6 +8,7 @@ from OpenGL.GL import *
 from OpenGL.GLU import *
 from pygame.locals import *
 import scipy.integrate
+from draw import drawGL
 # import warnings
 # warnings.filterwarnings("error", category=DeprecationWarning)
 
@@ -36,50 +37,11 @@ center_y_0 = 0
 inc = 0.075
 
 
-def circle(radius, posx, posy, n, epoch=0):
-    slides = 100
-    glLineWidth(0.05)
-    glColor3f(0.4, 0.4, 0.4)
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glEnable(GL_LINE_SMOOTH)
-    glBegin(GL_LINE_STRIP)
-    x = y = 0
-    for i in range(1, slides):
-        x = (radius/adjustX * math.cos(n*2*pi*i/slides + epoch) + posx)
-        y = (radius/adjustY * math.sin(n*2*pi*i/slides + epoch) + posy)
-        glVertex2f(x, y)
-    glEnd()
-    glColor3f(1.0, 1.0, 1.0)
 
-
-def point(x, y, size=2.0):
-    glEnable(GL_POINT_SMOOTH)
-    glEnable(GL_BLEND)
-    glPointSize(size)
-    glBegin(GL_POINTS)
-    glVertex2f(x, y)
-    glEnd()
-
-
-def line(a, b, width=1):
-    glLineWidth(width)
-    glBegin(GL_LINES)
-    glVertex2f(a[0], a[1])
-    glVertex2f(b[0], b[1])
-    glEnd()
-    glLineWidth(1)
-
-
-def curve(l):
-    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
-    glEnable(GL_LINE_SMOOTH)
-    glBegin(GL_LINE_STRIP)
-    for i in range(len(l)):
-        glVertex2f(i*inc*0.05, l[i])
-    glEnd()
 
 
 def main():
+    draw = drawGL(adjustX, adjustY, inc)
     pygame.init()
     inp = input(
         'Enter the function with limits (separated by space):\n').split(' ')
@@ -136,7 +98,7 @@ def main():
                 quit()
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-        line((1, 0), (-1, 0))
+        draw.line((1, 0), (-1, 0))
         x = center_x_0
         y = center_y_0 + (c/2)/scale
 
@@ -149,22 +111,22 @@ def main():
             x += radius/adjustX * math.cos(-i*k*arc+epoch)
             y += radius/adjustY * math.sin(-i*k*arc+epoch)
 
-            circle(radius, prevx, prevy, i, epoch)
-            point(prevx, prevy)
+            draw.circle(radius, prevx, prevy, i, epoch)
+            draw.point(prevx, prevy)
 
-            line((prevx, prevy), (x, y))
+            draw.line((prevx, prevy), (x, y))
 
         l.insert(0, y)
         if len(l) > 300:
             l.pop()
-        point(0, l[0], 5)
-        line((0, l[0]), (x, y))
-        line((0, l[0]), (0, 0), 0.5)
-        line((x, 0), (x, y), 0.5)
+        draw.point(0, l[0], 5)
+        draw.line((0, l[0]), (x, y))
+        draw.line((0, l[0]), (0, 0), 0.5)
+        draw.line((x, 0), (x, y), 0.5)
 
         arc += inc
 
-        curve(l)
+        draw.curve(l)
 
         pygame.display.flip()
 
