abf0346a1c9432a1244eb471fab42d6a56503c80
Saptarshi Dutta <quantumcosmos7@gmail.com>
2020-09-12 17:22:42 +0530

    REFACTOR_2: Organizing imports in 'fourier.py'

--- FSG/fourier.py
+++ FSG/fourier.py
@@ -1,16 +1,11 @@
-import math
 import pygame
-# from sympy import Symbol
-from scipy import *
 import numpy as np
-from time import time
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from pygame.locals import *
-import scipy.integrate
 from draw import drawGL
-# import warnings
-# warnings.filterwarnings("error", category=DeprecationWarning)
+from scipy.integrate import quad
+from math import pi, atan2, sqrt, sin, cos
+from pygame.locals import DOUBLEBUF, OPENGL
+from OpenGL.GL import glClear, GL_COLOR_BUFFER_BIT, GL_DEPTH_BUFFER_BIT
+
 
 
 math_tray = {
@@ -24,27 +19,19 @@ math_tray = {
     "exp": "np.exp",
 }
 
-pi = math.pi
 
 display = (800, 500)
-adjustX = display[0]/min(display)
-adjustY = display[1]/min(display)
 
 
-center_x_0 = -0.55
-center_y_0 = 0
+center = (-0.55, 0)
+max_r_for_inf_cond = 0.5
 
 inc = 0.075
 
 
-
-
-
 def main():
-    draw = drawGL(adjustX, adjustY, inc)
     pygame.init()
-    inp = input(
-        'Enter the function with limits (separated by space):\n').split(' ')
+    inp = input('Enter the function with limits (separated by space):\n').split(' ')
     str_func = inp.pop(0)
     lim = 50
     arc = 0
@@ -54,27 +41,23 @@ def main():
         if i in str_func:
             str_func = str_func.replace(i, math_tray[i]).replace("np.np.", "np.")
 
-    uL = eval(max(inp))
-    lL = eval(min(inp))
+    lL = eval(inp[0])
+    uL = eval(inp[1])
     temp = str_func
     max_val = float(eval(temp.replace('x', str(uL))))
 
     R = uL-lL
-    ai = lambda n: scipy.integrate.quad(lambda x: (2/R)*eval(str_func)*np.cos(2*pi*x*n/R), lL, uL)[0]
-    bi = lambda n: scipy.integrate.quad(lambda x: (2/R)*eval(str_func)*np.sin(2*pi*x*n/R), lL, uL)[0]
-    c = scipy.integrate.quad(lambda x: (2/R)*eval(str_func), lL, uL)[0]
+    ai = lambda n: quad(lambda x: (2/R)*eval(str_func)*np.cos(2*pi*x*n/R), lL, uL)[0]
+    bi = lambda n: quad(lambda x: (2/R)*eval(str_func)*np.sin(2*pi*x*n/R), lL, uL)[0]
+    c = quad(lambda x: (2/R)*eval(str_func), lL, uL)[0]
 
 
-    def rad(i):
-        a1 = (ai(i))**2
-        b1 = (bi(i))**2
-        return math.sqrt(a1 + b1)
+    def rad(i): return sqrt((ai(i))**2 + (bi(i))**2)
 
-    def e(i): return math.atan2(float(ai(i)), float(bi(i)))
+    def e(i): return atan2(float(ai(i)), float(bi(i)))
 
     list_rad = []
     list_e = []
-    max_r_for_inf_cond = 0.5
     for i in range(1, lim+1):
         r = rad(i)
         if r > 1e+5:
@@ -88,6 +71,9 @@ def main():
 
     print('Values Ready!')
 
+    adjustX = display[0]/min(display)
+    adjustY = display[1]/min(display)
+    draw = drawGL(adjustX, adjustY, inc)
     pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
 
     while True:
@@ -99,8 +85,8 @@ def main():
 
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
         draw.line((1, 0), (-1, 0))
-        x = center_x_0
-        y = center_y_0 + (c/2)/scale
+        x = center[0]
+        y = center[1] + (c/2)/scale
 
         for i in range(1, len(list_rad)+1):
             prevx = x
@@ -108,12 +94,11 @@ def main():
             epoch = list_e[i-1]
 
             radius = (list_rad[i-1])
-            x += radius/adjustX * math.cos(-i*k*arc+epoch)
-            y += radius/adjustY * math.sin(-i*k*arc+epoch)
+            x += radius/adjustX * cos(-i*k*arc+epoch)
+            y += radius/adjustY * sin(-i*k*arc+epoch)
 
             draw.circle(radius, prevx, prevy, i, epoch)
             draw.point(prevx, prevy)
-
             draw.line((prevx, prevy), (x, y))
 
         l.insert(0, y)
