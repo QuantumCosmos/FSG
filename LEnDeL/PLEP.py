3eb2ca879674127d40781fb7249df4a26faede60
Saptarshi Dutta <quantumcosmos7@gmail.com>
2020-09-21 14:58:39 +0530

    test for setup.exe

--- LEnDeL/PLEP.py
+++ LEnDeL/PLEP.py
@@ -1,3 +1,4 @@
+import os
 from sympy import *
 start = ['\\documentclass{article}\n',
          '\\usepackage[utf8]{inputenc}\n',
@@ -11,14 +12,17 @@ tail = '\n\\end{equation*}\n'
 equn = '\\alpha\\cdot\\gamma+\\beta = x^{\\frac{1!}{n}+\\frac{2!}{n^2}+\\frac{3!}{n^3}+\\ldots}'
 x = symbols('x')
 # inp = x**(12+x+3**(4*x+x))+x*3+46
-# inp = x**12+1
-# inp = sin(x)**(25*x+4**x*(90+x))+exp(2**(log(x**(2*x+6))+10*x**x))
+# inp = x**12+1# inp = sin(x)**(25*x+4**x*(90+x))+exp(2**(log(x**(2*x+6))+10*x**x))
 inp = 2*x +6
+inp = input('Enter math:\n')
 inp = simplify(inp)
 inp = latex(inp)
+print(inp)
 texdoc.append(''.join(start))
 texdoc.append(head+equn+tail+head+inp+tail)
 texdoc.append(''.join(end))
 with open('new.tex', 'w') as fout:
     for i in range(len(texdoc)):
         fout.write(texdoc[i])
+
+os.system("windll\\pdflatex new.tex")
