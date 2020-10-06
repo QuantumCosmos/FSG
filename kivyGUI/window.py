724ac5dcdd6dfb29cc30086b612e17508501fd9e
Saptarshi Dutta <quantumcosmos7@gmail.com>
2020-10-06 20:02:06 +0530

    test commands added

--- kivyGUI/window.py
+++ kivyGUI/window.py
@@ -1,14 +1,18 @@
 from kivy.uix.screenmanager import Screen
-from kivy.uix.textinput import TextInput
 from kivy.app import App
 from kivy.core.window import Window
-from kivy.properties import ObjectProperty
 
 class User(Screen):
 
     def __init__(self, **kwargs):
         super(User, self).__init__(**kwargs)
         Window.bind(on_key_down=self._on_keyboard_down)
+        self.command_dict =  {
+            'one': self.command_one,
+            'two': self.command_two,
+            'three': self.command_three,
+            'exit': self.command_exit,
+        }
 
     def _on_keyboard_down(self, instance, keyboard, keycode, text, modifiers):
         if self.ids.test3.focus and keycode == 40:
@@ -21,6 +25,32 @@ class User(Screen):
     def set_prompt(self):
         self.ids.test1.text += '\n>>> '
         self.ids.test3.text = self.ids.test1.text.split('\n')[-2].replace('>>>', '')
+        self.call_command()
+
+    
+    def call_command(self):
+        t = self.ids.test3.text.replace(' ', '')
+        try:
+            self.command_dict[t](t)
+        except KeyError:
+            print('Sorry, there is no command key: ' + t)
+
+    
+    def command_one(self, t):
+        print("Command one has Been Processed")
+
+
+    def command_two(self, t):
+        print("Command two has Been Processed")
+
+
+    def command_three(self, t):
+        print("Command three has Been Processed")
+    
+    def command_exit(self, t):
+        App.get_running_app().stop()
+
+
 
 
 class Consol_Window(App):
